d7f83621c3a646b7afadc650a3d104cc3622a7f6
Chuck <wangjohn419@gmail.com>
2026-09-11 11:52:54 -0700

    Export inference runtime APIs

--- src/llm_inference/__init__.py
+++ src/llm_inference/__init__.py
@@ -9,12 +9,14 @@ from .config import (
     TransformersConfig,
     VLLMConfig,
 )
+from .factory import create_backend
+from .inspection import InferencePlan, build_inference_plan, format_inference_plan
 from .request import GenerationConfig, GenerationRequest
 from .result import GenerationCandidate, GenerationResult
+from .run import InferenceRunIdentity, create_run_identity
 from .structured import StructuredOutputConfig
 
 __all__ = [
-    "AsyncInferenceBackend",
     "BackendCapabilities",
     "BatchConfig",
     "GenerationCandidate",
@@ -23,9 +25,15 @@ __all__ = [
     "GenerationResult",
     "InferenceBackend",
     "InferenceConfig",
+    "InferencePlan",
+    "InferenceRunIdentity",
     "ModelConfig",
     "StructuredOutputConfig",
     "TransformersConfig",
     "VLLMConfig",
+    "build_inference_plan",
+    "create_backend",
+    "create_run_identity",
+    "format_inference_plan",
     "validate_request_capabilities",
 ]
